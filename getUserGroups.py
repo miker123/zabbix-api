7d04c0374630daf5dde3532a8dd7c4f21ad5a253
Mike R <mikeup28@gmail.com>
2017-10-15 16:35:07 -0700

    Update getUserGroups.py

--- getUserGroups.py
+++ getUserGroups.py
@@ -9,6 +9,8 @@ import sys
 ZABBIX_SERVER = 'Zabbix Web Interface URL'
 
 zapi = ZabbixAPI(ZABBIX_SERVER)
+#if getting certification errors, the line below is required
+zapi.session.verfy=False 
 
 # Login to the Zabbix API
 #replace username and password with the appropriate data. Keep the quotes around the data.
